1313089a40ef432626e7f7ff21a3bb8da5797af8
anurag-world <anurocks91@gmail.com>
2021-03-24 01:42:02 +0530

    Update

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const AboutPage = () => (
+  <>
+    <h1>About Me</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Netus et malesuada
+      fames ac turpis egestas integer eget. Iaculis eu non diam phasellus
+      vestibulum lorem. Aenean euismod elementum nisi quis eleifend quam
+      adipiscing vitae proin. Ac tortor vitae purus faucibus ornare suspendisse
+      sed. Senectus et netus et malesuada fames ac turpis egestas. Mi in nulla
+      posuere sollicitudin aliquam. Amet dictum sit amet justo. Porta lorem
+      mollis aliquam ut porttitor leo. Ut venenatis tellus in metus. Adipiscing
+      at in tellus integer feugiat scelerisque varius morbi enim. Etiam sit amet
+      nisl purus in. Enim facilisis gravida neque convallis a cras. Fermentum et
+      sollicitudin ac orci phasellus egestas tellus rutrum. Vehicula ipsum a
+      arcu cursus. In hac habitasse platea dictumst vestibulum rhoncus est
+      pellentesque.
+    </p>
+
+    <p>
+      Est placerat in egestas erat imperdiet sed euismod nisi porta. Arcu ac
+      tortor dignissim convallis aenean et tortor. In hac habitasse platea
+      dictumst vestibulum rhoncus est pellentesque. Purus semper eget duis at
+      tellus at urna condimentum. Ipsum nunc aliquet bibendum enim facilisis
+      gravida neque convallis. Semper eget duis at tellus at urna condimentum
+      mattis pellentesque. Turpis egestas integer eget aliquet. Ultrices mi
+      tempus imperdiet nulla malesuada. In cursus turpis massa tincidunt dui ut.
+      Amet aliquam id diam maecenas ultricies mi eget mauris. Risus ultricies
+      tristique nulla aliquet enim tortor at auctor urna. Mattis aliquam
+      faucibus purus in massa tempor nec. Gravida in fermentum et sollicitudin
+      ac. Purus viverra accumsan in nisl. Aliquam purus sit amet luctus
+      venenatis lectus magna. Pellentesque habitant morbi tristique senectus.
+      Blandit cursus risus at ultrices mi tempus imperdiet nulla. Sed velit
+      dignissim sodales ut. Pretium nibh ipsum consequat nisl vel pretium lectus
+      quam. Venenatis cras sed felis eget.
+    </p>
+
+    <p>
+      Massa id neque aliquam vestibulum morbi blandit cursus. Aliquam malesuada
+      bibendum arcu vitae elementum curabitur. Elit sed vulputate mi sit amet
+      mauris commodo quis imperdiet. Ut venenatis tellus in metus vulputate eu
+      scelerisque felis. Morbi tristique senectus et netus et. Vel orci porta
+      non pulvinar. Quis varius quam quisque id diam vel quam elementum. Integer
+      eget aliquet nibh praesent tristique magna sit amet purus. Massa tincidunt
+      dui ut ornare lectus sit amet est placerat. Dignissim diam quis enim
+      lobortis scelerisque fermentum. Libero justo laoreet sit amet cursus sit
+      amet. Elit ut aliquam purus sit amet luctus venenatis. Vitae tempus quam
+      pellentesque nec. Non consectetur a erat nam at lectus urna duis
+      convallis. Pellentesque elit eget gravida cum sociis natoque penatibus et.
+      Et egestas quis ipsum suspendisse ultrices. Blandit cursus risus at
+      ultrices. Quam quisque id diam vel. Massa tempor nec feugiat nisl pretium.
+    </p>
+  </>
+)
+
+export default AboutPage
